09d8cd282016023d87cccf700d79eecf8e73ff31
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2026-08-08 21:48:49 +0200

    Fix last day bug

--- Assets/Scripts/Objects/MaterialChanger.cs
+++ Assets/Scripts/Objects/MaterialChanger.cs
@@ -10,7 +10,7 @@ public class MaterialChanger : MonoBehaviour
 
     private MeshRenderer renderer;
 
-    private void Start()
+    private void Awake()
     {
         renderer = GetComponent<MeshRenderer>();
     }
