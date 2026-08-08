0c232922fb281d253bbce24e6bbff277e495eec2
Pojemnik <andrzej.gauza@outlook.com>
2026-08-08 08:57:27 +0200

    Make meat grinding work

--- Assets/Scripts/Minigames/MeatGrinding.cs
+++ Assets/Scripts/Minigames/MeatGrinding.cs
@@ -10,13 +10,19 @@ public class MeatGrinding : Minigame
 
     public override string Name => "Grind";
 
-    private float currentAngle = 0f;
+    private float rotatedAngle = 0f;
+    private bool pressed = false;
+    private float lastAngle;
+    private Vector2 screenHalf;
 
     public override void StartMinigame()
     {
         base.StartMinigame();
+        screenHalf = new Vector2(Screen.width / 2f, Screen.height / 2f);
 
         InputAdapter.look.performed += OnMouseMove;
+        InputAdapter.interact.started += OnMousePress;
+        InputAdapter.interact.canceled += OnMouseRelease;
     }
 
     public override void EndMinigame(bool win)
@@ -24,11 +30,53 @@ public class MeatGrinding : Minigame
         base.EndMinigame(win);
 
         InputAdapter.look.performed -= OnMouseMove;
-        currentAngle = 0f;
+        InputAdapter.interact.started -= OnMousePress;
+        InputAdapter.interact.canceled -= OnMouseRelease;
+        rotatedAngle = 0f;
     }
 
     private void OnMouseMove(InputAction.CallbackContext context)
     {
-        Debug.Log(context.ReadValue<Vector2>());
+        float angle = GetMouseAngle();
+        float diff = GetAngleDelta(angle, lastAngle);
+        if (pressed)
+        {
+            rotatedAngle += diff;
+            Debug.Log(rotatedAngle);
+            if (rotatedAngle >= grindAngle)
+            {
+                EndMinigame(true);
+            }
+        }
+        lastAngle = angle;
+    }
+
+    private float GetAngleDelta(float currentAngle, float lastAngle)
+    {
+        if (lastAngle - currentAngle > 0f && lastAngle - currentAngle < 180f)
+        {
+            return lastAngle - currentAngle;
+        }
+        if (currentAngle > 270 && lastAngle < 90)
+        {
+            return currentAngle - 360 - lastAngle;
+        }
+        return 0f;
+    }
+
+    private void OnMouseRelease(InputAction.CallbackContext context)
+    {
+        pressed = false;
+    }
+
+    private void OnMousePress(InputAction.CallbackContext context)
+    {
+        pressed = true;
+        lastAngle = GetMouseAngle();
+    }
+
+    private float GetMouseAngle()
+    {
+        return Vector2.SignedAngle(Vector2.up, Mouse.current.position.ReadValue() - screenHalf) + 180f;
     }
 }
