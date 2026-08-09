da67dda25daf21c5683fd5541e419311d6ba7f6a
Pojemnik <andrzej.gauza@outlook.com>
2026-08-09 02:25:37 +0200

    Make watch work correctly

--- Assets/Scripts/Player/PlayerWatchHandler.cs
+++ Assets/Scripts/Player/PlayerWatchHandler.cs
@@ -1,17 +1,20 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(TMPro.TextMeshProUGUI))]
 public class PlayerWatchHandler : MonoBehaviour
 {
-    private TMPro.TextMeshProUGUI text;
+    private TMPro.TextMeshPro text;
 
     private void Start()
     {
-        text = GetComponent<TMPro.TextMeshProUGUI>();
+        text = GetComponent<TMPro.TextMeshPro>();
     }
 
     private void Update()
     {
-        
+        float shiftNormlizedTime = GameManager.Instance.shiftTime / GameManager.Instance.ShiftDuration;
+        int watchTime = (int)(shiftNormlizedTime * 8f * 60f) + 22 * 60;
+        text.text = $"{(watchTime/60 % 24).ToString().PadLeft(2, '0')}\n{(watchTime%60).ToString().PadLeft(2, '0')}";
     }
 }
