f8e0d0ef65f4b7c47998dc9a7984d84518b47a5f
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2026-08-09 11:19:48 +0200

    Bulehc

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,4 +1,6 @@
 using StorkStudios.CoreNest;
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,13 +10,39 @@ using SceneEnum = StorkStudios.CoreNest.Scene;
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField]
-    private TextMeshProUGUI continueLabel;
+    private List<TextMeshProUGUI> continueLabels;
+    [SerializeField]
+    private GameObject normalMenu;
+    [SerializeField]
+    private GameObject scaryMenu;
+    [SerializeField]
+    private RangeBoundariesFloat normalDuration;
+    [SerializeField]
+    private RangeBoundariesFloat scaryDuration;
 
     private void Start()
     {
-        continueLabel.text = $"Continue: Day {ShiftDays.Instance.CurrentDayNumber}";
+        continueLabels.ForEach(label => label.text = $"Continue: Day {ShiftDays.Instance.CurrentDayNumber}");
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+
+        StartCoroutine(MenuCoroutine());
+    }
+
+    private IEnumerator MenuCoroutine()
+    {
+        while (true)
+        {
+            normalMenu.SetActive(true);
+            scaryMenu.SetActive(false);
+
+            yield return new WaitForSeconds(normalDuration.GetRandomBetween());
+
+            normalMenu.SetActive(false);
+            scaryMenu.SetActive(true);
+
+            yield return new WaitForSeconds(scaryDuration.GetRandomBetween());
+        }
     }
 
     public void LoadGameScene()
